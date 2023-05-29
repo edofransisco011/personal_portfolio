3c97ad45bb0e8a9359a92d3d40f1c336284794b5
edofransisco011 <edofransisco011@gmail.com>
2023-05-29 08:06:06 +0700

    about & main update

--- components/About.jsx
+++ components/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
           </p>
           <Link href='/#projects'>
             <button className='px-8 mr-8 py-2 mt-4 flex:auto'>
-              Check out some of my latest projects 👌
+              Check out some of my latest projects
             </button>
           </Link>
         </div>
